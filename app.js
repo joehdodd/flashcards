63d1008fc833f57d60e79e824f0b35dd973ed0f2
Joe <joehdodd@gmail.com>
2018-01-19 12:08:50 -0500

    install pug, create view files, update app.js

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const express = require('express');
-
 const app = express();
 
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
-  res.send('I love web Wiley!')
+  res.render('index');
 });
+
+app.get('/cards', (req, res) => {
+  // res.locals.colors = [ // one way to define a variable LOCAL to this particular route
+  //   'red',
+  //   'orange',
+  //   'yellow',
+  //   'green',
+  //   'blue',
+  //   'purple'
+  // ];
+  res.render('card', { hint: `Think about whose tomb it is.`  }); // first arg is the .pug file we want to render, second arg can be an object of variables local to the file
+})
 
 app.listen(3000);
