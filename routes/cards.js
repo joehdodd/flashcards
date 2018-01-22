df3946e0a52f2b79225d19c688831d9f106853e0
Joe <joehdodd@gmail.com>
2018-01-22 13:42:05 -0500

    serving up different cards, random cards and switching between cards

--- routes/cards.js
+++ routes/cards.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const { data } = require('../data/flashcardData.json');
+const { cards } = data;
 
 router.get('/', (req, res) => {
-  res.render('card', { hint: `Think about whose tomb it is.`  }); // first arg is the .pug file we want to render, second arg can be an object of variables local to the file
+  const numberOfCards = cards.length;
+  const flashcardId = Math.floor( Math.random() * numberOfCards );
+  res.redirect(`cards/${flashcardId}`);
+});
+
+router.get('/:id', (req, res) => {
+  const { side } = req.query;
+  const { id } = req.params;
+  const text = cards[id][side];
+  const { hint } = cards[id];
+  let sideToShow = side === 'question' ? 'answer' : 'question';
+  let sideToShowDisplay = side === 'question' ? 'Answer' : 'Question';
+  const templateData = { id, text, sideToShow, sideToShowDisplay };
+  
+  if (!side) res.redirect(`/cards/${id}?side=question`);
+  if (side === 'question') templateData.hint = hint;
+  res.render('card', templateData); // first arg is the .pug file we want to render, second arg can be an object of variables local to the file
 });
 
 module.exports = router;
